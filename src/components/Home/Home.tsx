51075a7ee8a675d20d237b29daf836ae004c8474
v-nrenold <v-nrenold@microsoft.com>
2025-02-20 11:00:58 +0530

    office changes

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,13 +2,16 @@ import productJson from "../../MockData/product.json";
 
 function Home() {
   let productList: product[] = productJson;
+  const filter = sessionStorage.getItem('filter');
+  productList = productList.filter((e) =>);
   return (
     <>
       <div>
         {productList.map((e) => {
+          const temp = "./product?" + e.productId;
           return (
             <div>
-              <div>{e.productName}</div>
+              <a href={temp}>{e.productName}</a>
               <img alt="Image failed to load" src={e.imageUrl}></img>
             </div>
           );
@@ -20,7 +23,34 @@ function Home() {
 
 export default Home;
 
-interface product {
+export interface product {
+  [key: string] :any; 
+  productId : string;
   productName: string;
   imageUrl: string;
+  productPrice : number;
+  size : number;
+  material : string;
 }
+
+interface filterValues{
+  field : any
+  operation : string
+  value :string
+}
+function processFilter(filter :filterValues, products :product[])
+{  
+  switch(filter.operation){
+    case ">":
+      return products.filter((e) => e[filter.field] > filter.value)
+      break;
+      case "<":
+        return products.filter((e) => e[filter.field] < filter.value)
+        break;
+        case "=":
+      return products.filter((e) => e[filter.field] === filter.value)
+      break;
+}
+}
+
+
--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -1,7 +1,20 @@
+import productList from '../../MockData/product.json'
+import { product as productInfo } from '../Home/Home'
+
 function Product()
 {
+    const product :productInfo = productList.filter((e) => e.productId == window.location.search.substring(1))[0]
     return(
-        <>product</>
+        <>
+        <div>        
+        {product.productName}:
+        <img alt="Image failed to load" src={product.imageUrl}></img>
+        <div>Product Details</div>
+        <div>Size     : {product.productPrice}</div>
+        <div>Material : {product.material}</div>
+        <div>Price    : {product.productPrice}</div>
+        </div>                
+        </>
     )
 }
 
