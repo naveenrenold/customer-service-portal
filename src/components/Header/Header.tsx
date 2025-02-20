5935ebe1b31acd53686547d4f9cbcabf04bfc70c
naveenrenold <naveenrenold@gmail.com>
2025-02-20 17:47:18 +0530

    commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
           <a href="/home">Home</a>
         </li>
         <li>
-          <a href="/product">Product</a>
+          <a className="disabled" href="/product">Product</a>
         </li>
         <li>
           <a href="/" onClick={LogOut}>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,22 @@
 import productJson from "../../MockData/product.json";
+import "./Home.css";
 
 function Home() {
   let productList: product[] = productJson;
   return (
     <>
-      <div>
+      <div className="flex wrap center">
         {productList.map((e) => {
           return (
-            <div>
-              <div>{e.productName}</div>
-              <img alt="Image failed to load" src={e.imageUrl}></img>
+            <div className="flex center">
+              <div className="flex flexcolumm center">
+                <div className="textalign">{e.productName}</div>
+                <img
+                  className="img"
+                  alt="Image failed to load"
+                  src={e.imageUrl}
+                ></img>
+              </div>
             </div>
           );
         })}
